b37b4f709e4a15d607ee55811e87e1707d53c878
Veddesh RGM <160174822+Veddesh@users.noreply.github.com>
2025-06-03 10:13:51 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -2,26 +2,24 @@
 
 
 module Testbench ();
-  reg Clock = 1'b0;  // Initialize here, only once
+  reg Clock = 1'b0; 
   reg Reset;
   wire [3:0] Count;
 
-  // instantiate the design under verification (DUV)
+  
   Mycounter I1(.CLK(Clock), .RST(Reset), .OUT(Count));
 
-  // initialize the testbench
   initial begin
-    $display("Starting simulation...");
-    Reset = 1'b1;      // reset at time=0
-    #100 Reset = 1'b0; // release reset at time=100
-    #2000 Reset = 1'b1;// assert reset again at time=2100
-    #400 $finish;      // finish simulation at time=2500
+    $display("Starting
+    #100 Reset = 1'b0; 
+    #2000 Reset = 1'b1;
+    #400 $finish;      
   end
 
-  // generate clock with period 100 time units
+ 
   always #50 Clock = ~Clock;
 
-  // monitor and dump waveforms
+
   initial begin
     $dumpfile("count.vcd");
     $dumpvars(0, Testbench);
